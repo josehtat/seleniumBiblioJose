120d5379b694ac4d1f1964454a75e28e75583845
josehtat <jhurtadot33@gmail.com>
2025-02-04 19:32:37 +0100

    avances 01-header y 02-login

--- .test/01-header.js
+++ .test/01-header.js
@@ -3,43 +3,31 @@ const { BaseTest } = require("./BaseTest.js")
 const { By, until } = require("selenium-webdriver");
 const assert = require('assert');
 
-//.env
-require('dotenv').config();
-
-// Access the environment variables
-const site_url = process.env.site_url;
-
-
 // heredem una classe amb un sol mètode test()
 // emprem this.driver per utilitzar Selenium
+require('dotenv').config();
+console.log(process.env);
 
-class MyTest extends BaseTest {
 
+class MyTest extends BaseTest
+{
     async test() {
         // testejem H1 a la home page
         //////////////////////////////////////////////////////
-
-        await this.driver.get(site_url+"/admin/login/");
-
+        var site = process.env.URL
+        await this.driver.get(site+"/admin/login/");
         var currentText = await this.driver.findElement(By.tagName("h1")).getText();
-
         var expectedText = "Administració de Django";
-
-        assert(currentText == expectedText, "Títol H1 de la pàgina principal incorrecte");
+        assert( currentText==expectedText, "Títol H1 de la pàgina principal incorrecte");
 
         console.log("TEST OK");
     }
-
 }
 
 // executem el test
 
 (async function test_example() {
-
     const test = new MyTest();
-
     await test.run();
-
     console.log("END")
-
 })();
--- .test/02-login-ok.js
+++ .test/02-login-ok.js
@@ -2,43 +2,46 @@
 const { BaseTest } = require("./BaseTest.js")
 const { By, until } = require("selenium-webdriver");
 const assert = require('assert');
-
+ 
 //.env
-require('dotenv').config();
-
-// Access the environment variables
-const site_url = process.env.site_url;
-const username = process.env.username;
-const password = process.env.password;
-
-// heredem una classe amb un sol mètode test()
-// emprem this.driver per utilitzar Selenium
-
-class MyTest extends BaseTest {
-        async test() {
-                // testejem H1 a la home page
-                //////////////////////////////////////////////////////
-                await this.driver.get(site_url+"/admin/login/");
-
-                // 2 posar usuari i pass
-                await this.driver.findElement(By.name("username")).sendKeys(username);
-                await this.driver.findElement(By.name("password")).sendKeys(password);
-
-                // 3 boto send .click()
-                await this.driver.findElement(By.xpath("//input[@value='Iniciar sessió']")).click();
-
-                // 4 cerrar sessió
-                await this.driver.sleep(1000);
-                await this.driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-                console.log("TEST OK");
-        }
+require('dotenv').config()
+console.log(process.env)
+ 
+class MyTest extends BaseTest
+{
+    async test() {
+        // Login test
+        //////////////////////////////////////////////////////
+        var site = process.env.URL
+        var driver = this.driver
+        await driver.get(site+"/admin/login/");
+ 
+        // 1 cercar login box
+        let usernameInput = await driver.wait(until.elementLocated(By.id('id_username')), 10000);
+        let passwordInput = await driver.wait(until.elementLocated(By.id('id_password')), 10000);
+ 
+        // 2 posar usuari i pass
+        usernameInput.sendKeys(process.env.USUARI)
+        passwordInput.sendKeys(process.env.CONTRASENYA)
+ 
+        // 3 boto send .click()
+        let sendButton = await driver.wait(until.elementLocated(By.css('input[value="Iniciar sessió"]')), 10000);
+        sendButton.click()
+ 
+        // 4 comprovem que hem entrat
+        let logoutButton = await driver.wait(until.elementLocated(By.xpath('//button[@type="submit"]')), 10000);
+        var currentLogoutText = await logoutButton.getText();
+        var expectedText = "FINALITZAR SESSIÓ";
+        assert( currentLogoutText==expectedText, "Login fallit.\n\tTEXT TROBAT="+currentLogoutText+"\n\tTEXT ESPERAT="+expectedText);
+ 
+        console.log("TEST OK");
+    }
 }
-
+ 
 // executem el test
-
+ 
 (async function test_example() {
-        const test = new MyTest();
-        await test.run();
-        console.log("END")
+    const test = new MyTest();
+    await test.run();
+    console.log("END")
 })();
